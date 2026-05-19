45c8a0eab7fe60321a4dc5d1bfa0df09ea549478
Oktawian <oktawianbieszke7@gmail.com>
2026-05-19 21:17:15 +0200

    feat: rewrite train.py using PyTorch equivalents
    Add accuracy and loss plot saving in results folder

--- src/train.py
+++ src/train.py
@@ -1,9 +1,14 @@
 import matplotlib.pyplot as plt
 import argparse
 import yaml
-from tensorflow.keras import optimizers
+import torch
+import os
+import datetime
+from tqdm import tqdm
+import torch.nn as nn
+from torch import optim
 from models.model_factory import get_model
-from dataset import get_generators
+from dataset import get_loaders
 
 
 def parse_args():
@@ -12,39 +17,113 @@ def parse_args():
     return parser.parse_args()
 
 
+def plot_training(history, model_name):
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    train_results_path = os.path.abspath(os.path.join(
+        current_dir, "..", "results", "train"))
+    os.makedirs(train_results_path, exist_ok=True)
+    timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+
+    plt.plot(history["accuracy"])
+    plt.plot(history['val_accuracy'])
+    plt.title("Model Accuracy")
+    plt.ylabel("Accuracy")
+    plt.xlabel("Epoch")
+    plt.legend(["Accuracy", "Validation Accuracy"])
+
+    acc_jpg_path = os.path.join(train_results_path,
+                                f"acc_{model_name}_{timestamp}.jpg")
+
+    plt.savefig(acc_jpg_path)
+    plt.show()
+
+    plt.plot(history['loss'])
+    plt.plot(history['val_loss'])
+    plt.title("Model Loss")
+    plt.ylabel("Loss")
+    plt.xlabel("Epoch")
+    plt.legend(["Loss", "Validation Loss"])
+
+    loss_jpg_path = os.path.join(train_results_path,
+                                 f"loss_{model_name}_{timestamp}.jpg")
+    plt.savefig(loss_jpg_path)
+    plt.show()
+
+
 def main():
     args = parse_args()
-
     with open(args.config, "r") as f:
         config = yaml.safe_load(f)
 
+    model_name = config["model_name"]
     image_size = tuple(config["image_size"])
     batch_size = config["batch_size"]
     epochs = config["epochs"]
+    steps_per_epoch = config["steps_per_epoch"]
+    steps_per_val = config["steps_per_epoch"]
     learning_rate = config["learning_rate"]
 
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
     model = get_model(
-        model_name=config["model_name"],
-        input_shape=(image_size[0], image_size[1], 3)
-    )
+        model_name=model_name,
+    ).to(device)
 
-    train_generator, val_generator = get_generators(image_size, batch_size)
+    train_loader, val_loader = get_loaders(image_size, batch_size)
 
-    model.compile(optimizers.Adam(learning_rate=learning_rate), loss='categorical_crossentropy',
-                  metrics=['accuracy'])
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    criterion = nn.CrossEntropyLoss()
 
-    hist = model.fit(train_generator, epochs=epochs,
-                     validation_data=val_generator, steps_per_epoch=2, validation_steps=2)
+    history = {"accuracy": [], "val_accuracy": [], "loss": [], "val_loss": []}
 
-    plt.plot(hist.history["accuracy"])
-    plt.plot(hist.history['val_accuracy'])
-    plt.plot(hist.history['loss'])
-    plt.plot(hist.history['val_loss'])
-    plt.title("model accuracy")
-    plt.ylabel("Accuracy")
-    plt.xlabel("Epoch")
-    plt.legend(["Accuracy", "Validation Accuracy", "Loss", "Validation Loss"])
-    plt.show()
+    for epoch in range(epochs):
+        model.train()
+        correct, total, running_loss = 0, 0, 0.0
+        train_steps = steps_per_epoch if steps_per_epoch else len(train_loader)
+        bar = tqdm(train_loader, desc=f"Epoch {epoch+1}/{epochs} [TRAIN]",
+                   total=train_steps)
+        for i, (inputs, labels) in enumerate(train_loader):
+            if steps_per_epoch and i >= steps_per_epoch:
+                break
+            inputs, labels = inputs.to(device), labels.to(device)
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+            correct += (outputs.argmax(1) == labels).sum().item()
+            total += labels.size(0)
+            bar.set_postfix(
+                loss=f"{running_loss/(i+1):.4f}", acc=f"{correct/total:.4f}", refresh=True)
+            bar.update(1)
+        history["loss"].append(
+            running_loss / train_steps)
+        history["accuracy"].append(correct/total)
+
+        model.eval()
+        correct, total, running_loss = 0, 0, 0.0
+        val_steps = steps_per_val if steps_per_val else len(val_loader)
+        bar = tqdm(val_loader, desc=f"Epoch {epoch+1}/{epochs} [VALID]",
+                   total=val_steps)
+        with torch.no_grad():
+            for i, (inputs, labels) in enumerate(val_loader):
+                if steps_per_val and i >= steps_per_val:
+                    break
+                inputs, labels = inputs.to(device), labels.to(device)
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
+                running_loss += loss.item()
+                correct += (outputs.argmax(1) == labels).sum().item()
+                total += labels.size(0)
+                bar.set_postfix(
+                    loss=f"{running_loss/(i+1):.4f}", acc=f"{correct/total:.4f}", refresh=True)
+                bar.update(1)
+        history["val_loss"].append(
+            running_loss / val_steps)
+        history["val_accuracy"].append(correct / total)
+
+    plot_training(history, model_name)
 
 
 if __name__ == "__main__":
