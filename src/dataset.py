23e8617d5add576b5df43a2b093005bd8a61ff6a
Oktawian <oktawianbieszke7@gmail.com>
2026-05-19 21:06:06 +0200

    feat: rewrite dataset.py using PyTorch equivalents
    ImageDataGenerator, flow_from_dataframe => Dataset, DataLoader

--- src/dataset.py
+++ src/dataset.py
@@ -1,39 +1,57 @@
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.preprocessing import image
 import pandas as pd
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
 import os
+from PIL import Image
 
 
-def get_generators(image_size=(224, 224), batch_size=32):
+class ImageDataset(Dataset):
+    def __init__(self, csv_path, images_dir, transform=None):
+        self.df = pd.read_csv(csv_path)
+        self.images_dir = images_dir
+        self.transform = transform
+        self.classes = sorted(self.df["race"].unique().tolist())
+        self.class_to_idx = {c: i for i, c in enumerate(self.classes)}
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        img_path = self.df.iloc[idx]["file"]
+        label = self.class_to_idx[self.df.iloc[idx]["race"]]
+        label = torch.tensor(label)
+        img = Image.open(os.path.join(
+            self.images_dir, img_path)).convert('RGB')
+        if self.transform:
+            img = self.transform(img)
+        return img, label
+
+
+def get_loaders(image_size=(224, 224), batch_size=32):
     current_dir = os.path.dirname(os.path.abspath(__file__))
     train_csv_path = os.path.abspath(os.path.join(
         current_dir, "..", "data", "train_labels.csv"))
     val_csv_path = os.path.abspath(os.path.join(
         current_dir, "..", "data", "val_labels.csv"))
-
-    train_df = pd.read_csv(train_csv_path)
-    val_df = pd.read_csv(val_csv_path)
-
-    train_datagen = ImageDataGenerator(rescale=1./255)
-    val_datagen = ImageDataGenerator(rescale=1./255)
-
     images_dir = os.path.abspath(os.path.join(
         current_dir, "..", "data"))
 
-    train_generator = train_datagen.flow_from_dataframe(train_df,
-                                                        directory=images_dir,
-                                                        x_col="file",
-                                                        y_col="race",
-                                                        target_size=image_size,
-                                                        batch_size=batch_size,
-                                                        class_mode="categorical",
-                                                        shuffle=True)
-    val_generator = val_datagen.flow_from_dataframe(val_df,
-                                                    directory=images_dir,
-                                                    x_col="file",
-                                                    y_col="race",
-                                                    target_size=image_size,
-                                                    batch_size=batch_size,
-                                                    class_mode="categorical",
-                                                    shuffle=True)
-    return train_generator, val_generator
+    transform = transforms.Compose([
+        transforms.Resize(image_size),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225]
+        )
+    ])
+
+    train_dataset = ImageDataset(train_csv_path, images_dir, transform)
+    val_dataset = ImageDataset(val_csv_path, images_dir, transform)
+
+    train_loader = DataLoader(
+        train_dataset, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(
+        val_dataset, batch_size=batch_size, shuffle=False)
+
+    return train_loader, val_loader
